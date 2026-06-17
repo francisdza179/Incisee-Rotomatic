97faacabf25d08800378eea64c2b4369617cb62b
francisdza179 <francisdza179@users.noreply.github.com>
2026-06-17 19:27:31 +0530

    Optimize and refactor CSS, add SEO meta tags, canonical link, schema markup, 404 page, robots.txt, and sitemap.xml

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const zlib = require('zlib');
 
 const PORT = 9090;
 const BASE = __dirname;
@@ -18,21 +19,66 @@ const MIME = {
   '.woff2': 'font/woff2',
 };
 
+function supportsGzip(acceptEncoding) {
+  return /\bgzip\b/.test(acceptEncoding || '');
+}
+
 const server = http.createServer((req, res) => {
   let urlPath = decodeURIComponent(req.url.split('?')[0]);
   if (urlPath.endsWith('/')) urlPath += 'index.html';
   const filePath = path.join(BASE, urlPath);
+
   try {
     const data = fs.readFileSync(filePath);
     const ext = path.extname(filePath);
-    res.writeHead(200, {
-      'Content-Type': MIME[ext] || 'text/plain',
-      'Access-Control-Allow-Origin': '*'
-    });
-    res.end(data);
+    const type = MIME[ext] || 'text/plain';
+
+    const acceptEncoding = req.headers['accept-encoding'];
+    if (supportsGzip(acceptEncoding)) {
+      zlib.gzip(data, (err, compressed) => {
+        if (err) {
+          res.writeHead(500);
+          return res.end('Compression error');
+        }
+        res.writeHead(200, {
+          'Content-Type': type,
+          'Content-Encoding': 'gzip',
+          'Access-Control-Allow-Origin': '*',
+        });
+        res.end(compressed);
+      });
+    } else {
+      res.writeHead(200, {
+        'Content-Type': type,
+        'Access-Control-Allow-Origin': '*',
+      });
+      res.end(data);
+    }
   } catch (e) {
-    res.writeHead(404);
-    res.end('Not found: ' + urlPath);
+    const filePath404 = path.join(BASE, '404.html');
+    try {
+      const data404 = fs.readFileSync(filePath404);
+      const acceptEncoding = req.headers['accept-encoding'];
+      if (supportsGzip(acceptEncoding)) {
+        zlib.gzip(data404, (err, compressed) => {
+          res.writeHead(404, {
+            'Content-Type': 'text/html',
+            'Content-Encoding': 'gzip',
+            'Access-Control-Allow-Origin': '*',
+          });
+          res.end(err ? data404 : compressed);
+        });
+      } else {
+        res.writeHead(404, {
+          'Content-Type': 'text/html',
+          'Access-Control-Allow-Origin': '*',
+        });
+        res.end(data404);
+      }
+    } catch (e2) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not found');
+    }
   }
 });
 
